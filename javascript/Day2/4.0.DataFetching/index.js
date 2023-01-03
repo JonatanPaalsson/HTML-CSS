92907a6b8255b90f82b505e97eaf18de50a10b4a
Jonatan pålsson <palssonjonatan@gmail.com>
2023-01-03 13:13:19 +0100

    added another way of data fetching

--- javascript/Day2/4.0.DataFetching/index.js
+++ javascript/Day2/4.0.DataFetching/index.js
@@ -1,9 +1,15 @@
 const button = document.querySelector("button")
 const img = document.querySelector("img")
-button.addEventListener("click", getCatPicture);
+button.addEventListener("click", getCatPic);
 
 function getCatPicture() {
     fetch("https://aws.random.cat/meow")
     .then(res => res.json())
     .then(data => img.src = data.file)
+}
+
+async function getCatPic() {
+    const cat = await fetch("https://aws.random.cat/meow").then(res => res.json())
+    img.src = cat.file
+    
 }
